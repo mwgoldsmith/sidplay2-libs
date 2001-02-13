e81790787ce6c5e1bb21172a1ba7f39b62066d63
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-02-13 19:41:17 +0000

    Support for forced loads added.  Latch is no longer buffered, which is the correct way according to docs.  Single shot mode added.

--- libsidplay/src/fake6526.cpp
+++ libsidplay/src/fake6526.cpp
@@ -16,61 +16,63 @@
  ***************************************************************************/
 /***************************************************************************
  *  $Log: not supported by cvs2svn $
+ *  Revision 1.4  2000/12/11 18:52:12  s_a_white
+ *  Conversion to AC99
+ *
  ***************************************************************************/
 
 #include "fake6526.h"
 
 fake6526::fake6526 ()
 {
-    defCount = 0xFFFF;
     reset ();
 }
 
 void fake6526::reset (void)
 {
-    locked = false;
-    _count = (setCount = defCount);
-    cra    = 0;
+    reset (0);
 }
 
 void fake6526::reset (uint_least16_t count)
 {
-    defCount = count;
-    reset ();
+    locked = false;
+    ta     = ta_latch = count;
+    cra    = 0;
 }
 
 uint8_t fake6526::read (uint_least8_t addr)
 {
-   if (addr > 0x0f) return 0;
+    if (addr > 0x0f) return 0;
 
-   switch (addr)
-   {
-   case 0x04: return (uint8_t) _count;
-   case 0x05: return (uint8_t) (_count >> 8);
-   default:   return regs[addr];
-   }
+    switch (addr)
+    {
+    case 0x4: return endian_16lo8 (ta_latch);
+    case 0x5: return endian_16hi8 (ta_latch);
+    default:  return regs[addr];
+    }
 }
 
 void fake6526::write (uint_least8_t addr, uint8_t data)
 {
-   static uint_least16_t latch = 0;
-   if (addr > 0x0f) return;
+    if (addr > 0x0f) return;
 
-   regs[addr] = data;
-   if (locked) return; // Stop programming changing time interval
-   switch (addr)
-   {
-   case 0x04:
-       latch = (uint_least16_t) data;
-   break;
-   case 0x05:
-       latch   |= (uint_least16_t) data << 8;
-       setCount = latch;
-   break;
-   case 0x0e:
-       cra = data & 0xef;  // (ms) mask strobe flag
-       if (cra & 0x01)
-           _count = setCount;
+    regs[addr] = data;
+    if (locked) return; // Stop programming changing time interval
+    switch (addr)
+    {
+    case 0x4:
+        endian_16lo8 (ta_latch, data);
+    break;
+    case 0x5:
+        endian_16hi8 (ta_latch, data);
+        if (!(cra & 0x01))
+            ta = ta_latch;
+    break;
+    case 0x0e:
+        // Check for forced load
+        if (data & 0x10)
+            ta = ta_latch;
+        cra = data & 0xef;  // (ms) mask strobe flag
    break;
    default:
    break;
