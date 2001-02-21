cf79435f3ca4e1e8b13a9a3adf1c6160e279f5f1
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-02-21 21:49:21 +0000

    Now uses new player::getErrorString function.

--- trunk/libsidplay/src/sidplay2.cpp
+++ trunk/libsidplay/src/sidplay2.cpp
@@ -16,6 +16,9 @@
  ***************************************************************************/
 /***************************************************************************
  *  $Log: not supported by cvs2svn $
+ *  Revision 1.4  2001/02/13 21:32:35  s_a_white
+ *  Windows DLL export fix.
+ *
  *  Revision 1.3  2001/02/07 20:57:08  s_a_white
  *  New SID_EXPORT define.  Supports SidTune now.
  *
@@ -103,7 +106,7 @@ void sidplay2::clockSpeed (sid2_clock_t clock, bool forced)
 {   sidplayer.clockSpeed (clock, forced); }
 
 const char *sidplay2::getErrorString (void)
-{   return sidplayer._errorString; }
+{   return sidplayer.getErrorString (); }
 
 int  sidplay2::fastForward  (uint_least8_t percent)
 {   return sidplayer.fastForward (percent); }
