975418be7699dbe7e5445a0c237c76bd884f4ed9
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-03-21 22:52:37 +0000

    Moved for better compatibility.

---
+++ sidplay/include/config.h
@@ -0,0 +1,31 @@
+/* Setup for Microsoft Visual C++ Version 5 */
+#ifndef _config_h_
+#define _config_h_
+
+/* Operating System */
+#define HAVE_MSWINDOWS
+
+/* Define if your C++ compiler implements exception-handling.  */
+/* #define HAVE_EXCEPTIONS 1 */
+
+/* Define if standard member ``ios::binary'' is called ``ios::bin''. */
+/* #define HAVE_IOS_BIN 1 */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if your processor stores words with the most significant
+   byte first (like Motorola and SPARC, unlike Intel and VAX).  */
+/* #define WORDS_BIGENDIAN */
+
+/* Define sound driver */
+/* #define HAVE_DIRECTX */
+#define HAVE_MMSYSTEM
+
+/* Name of package */
+#define PACKAGE "sidplay"
+
+/* Version number of package */
+#define VERSION "2.0.1"
+
+#endif // _config_h_
