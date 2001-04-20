dce79b3e841fdf5622f8b2bde66427d76b5c9b34
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-04-20 18:02:55 +0000

    Project files update.

--- trunk/libsidutils/include/config.h
+++ trunk/libsidutils/include/config.h
@@ -1,20 +1,25 @@
-/* Setup for Microsoft Visual C++ Version 5 */
+/* include/config.h.  Generated automatically by configure.  */
+/* include/config.h.in.  Generated automatically from configure.in by autoheader.  */
+/* config.h (template) */
 #ifndef _config_h_
 #define _config_h_
 
-#define PACKAGE "libsidutils"
-#define VERSION "0.0.0"
+/* @FOO@ : Define or undefine value FOO as appropriate. */
 
 /* Operating System */
-#define HAVE_MSWINDOWS
+#define HAVE_UNIX
 
 /* Define if your C++ compiler implements exception-handling.  */
-/* #define HAVE_EXCEPTIONS */
+#define HAVE_EXCEPTIONS 1
 
-/* Define if you support file names longer than 14 characters.  */
-#define HAVE_LONG_FILE_NAMES
+/* Define if your processor stores words with the most significant
+   byte first (like Motorola and SPARC, unlike Intel and VAX).  */
+/* #undef WORDS_BIGENDIAN */
 
-/* Define if you have the <strstrea.h> header file.  */
-#define HAVE_STRSTREA_H
+/* Name of package */
+#define PACKAGE "libsidutils"
 
-#endif // _config_h_
+/* Version number of package */
+#define VERSION "1.0.1"
+
+#endif /* _config_h_ */
