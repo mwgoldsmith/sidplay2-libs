c8ec474531cb5e5f6a56128414fa210f2893f8f8
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-07-03 17:53:29 +0000

    Added call to get pointer to current music buffer.

--- trunk/sidplay/src/audio/AudioBase.h
+++ trunk/sidplay/src/audio/AudioBase.h
@@ -16,6 +16,9 @@
  ***************************************************************************/
 /***************************************************************************
  *  $Log: not supported by cvs2svn $
+ *  Revision 1.1  2001/01/08 16:41:43  s_a_white
+ *  App and Library Seperation
+ *
  *  Revision 1.4  2000/12/11 19:07:14  s_a_white
  *  AC99 Update.
  *
@@ -35,17 +38,22 @@ protected:
     void       *_sampleBuffer;
 
 public:
-    AudioBase () { _errorString = "None"; }
+    AudioBase ()
+    {
+        _errorString  = "None";
+        _sampleBuffer = NULL;
+    }
     virtual ~AudioBase () {;}
 
     // All drivers must support these
-    virtual void *open  (AudioConfig &cfg) = 0;
+    virtual void *open  (AudioConfig &cfg, const char *name) = 0;
     // Rev 1.3 (saw) - Definition is incorrect and has been updated.
     // On a reset hardware buffers may have changed and therefore a
     // new address needs to be returned by the driver.
     virtual void *reset () = 0;
     virtual void *write () = 0;
     virtual void  close () = 0;
+    void   *buffer () { return _sampleBuffer; }
 
     void getConfig (AudioConfig &cfg) const
     {
