185590c369b283b177244dd6036c2b8776244092
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-07-14 13:03:33 +0000

    Now uses new component classes and event generation.

--- libsidplay/src/mos6526/mos6526.h
+++ libsidplay/src/mos6526/mos6526.h
@@ -16,6 +16,9 @@
  ***************************************************************************/
 /***************************************************************************
  *  $Log: not supported by cvs2svn $
+ *  Revision 1.4  2001/03/25 19:50:16  s_a_white
+ *  Timer B counts timer Aer a underflows correc
+ *
  *  Revision 1.3  2001/03/23 23:20:29  s_a_white
  *  Removed redundant reset prototype.
  *
@@ -44,54 +47,88 @@
 #ifndef _mos6526_h_
 #define _mos6526_h_
 
-#include "config.h"
-#include "sidtypes.h"
-#include "sidendian.h"
-#include "sidenv.h"
+#include "component.h"
 
-class MOS6526: public C64Environment
+class MOS6526: public component
 {
+private:
+    static const char *credit;
+
 protected:
     uint8_t regs[0x10];
+    bool    cnt_high;
 
     // Timer A
-    uint8_t cra;
+    uint8_t cra, cra_latch;
     uint_least16_t ta, ta_latch;
+    int  ta_state;
 
     // Timer B
     uint8_t crb;
     uint_least16_t tb, tb_latch;
 
     uint8_t icr, idr; // Interrupt Control Register
-    const bool nmi;
+    event_clock_t m_accessClk;
+    EventContext &event_context;
+
+    class EventTa: public Event
+    {
+    private:
+        MOS6526 &m_cia;
+        void event (void) {m_cia.ta_event ();}
+
+    public:
+        EventTa (MOS6526 *cia)
+            :Event("CIA Timer A"),
+             m_cia(*cia) {}
+    } event_ta;
+
+    /*
+    class EventStateMachineA: public Event
+    {
+    private:
+        MOS6526 &m_cia;
+        void event (void) {m_cia.cra_event ();}
+
+    public:
+        EventStateMachineA (MOS6526 *cia)
+            :Event("CIA Timer A (State Machine)"),
+             m_cia(*cia) {}
+    } event_stateMachineA;
+*/
+    class EventTb: public Event
+    {
+    private:
+        MOS6526 &m_cia;
+        void event (void) {m_cia.tb_event ();}
+
+    public:
+        EventTb (MOS6526 *cia)
+            :Event("CIA Timer B"),
+             m_cia(*cia) {}
+    } event_tb;
+
+    friend EventTa;
+//    friend EventStateMachineA;
+    friend EventTb;
 
 protected:
-    bool ta_clock (void);
-    void tb_clock (bool ta_underflow);
-    void trigger  (int interrupt);
+    MOS6526 (EventContext *context);
+    void ta_event  (void);
+    void tb_event  (void);
+    void trigger   (int irq);
+//    void stateMachineA_event (void);
 
+    // Environment Interface
+    virtual void interrupt (const bool state) = 0;
+    
 public:
-    MOS6526  (const bool isnmi);
-    ~MOS6526 ();
-
-    //Common:
-    void    clock (void);
+    // Component Standard Calls
     void    reset (void);
-    uint8_t read  (uint_least8_t  addr);
-    void    write (uint_least8_t  addr, uint8_t data);
+    uint8_t read  (const uint_least8_t addr);
+    void    write (const uint_least8_t addr, const uint8_t data);
+    const   char *credits (void) {return credit;}
 };
 
-inline void MOS6526::clock (void)
-{
-    bool ta_underflow = false;
-    if ((cra & 0x21) == 0x01)
-        ta_underflow  = ta_clock ();
-
-    if (crb & 0x01)
-    {
-        if ((crb & 0x61) != 0x21)
-            tb_clock (ta_underflow);
-    }
-}
-
 #endif // _mos6526_h_
+
