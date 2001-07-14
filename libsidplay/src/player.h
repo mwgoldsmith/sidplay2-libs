eb587173927e1bb2ab3a07050fbb42ba9d8d5ac8
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-07-14 12:50:58 +0000

    Support for credits and debuging.  External filter selection removed.  RTC and samples obtained in a more efficient way.  Support for component and sidbuilder classes.

--- libsidplay/src/player.h
+++ libsidplay/src/player.h
@@ -16,6 +16,9 @@
  ***************************************************************************/
 /***************************************************************************
  *  $Log: not supported by cvs2svn $
+ *  Revision 1.12  2001/04/23 17:09:56  s_a_white
+ *  Fixed video speed selection using unforced/forced and NTSC clockSpeeds.
+ *
  *  Revision 1.11  2001/03/22 22:45:20  s_a_white
  *  Re-ordered initialisations to match defintions.
  *
@@ -58,13 +61,15 @@
 #include "config.h"
 #include "sidplay2.h"
 #include "sidenv.h"
-#include "mos6510/mos6510.h"
-#include "mos6526/mos6526.h"
-#include "mos656x/mos656x.h"
-#include "mos6581/mos6581.h"
-#include "xsid/xsid.h"
+#include "c64env.h"
+#include "c64/c64sid.h"
+#include "c64/c64xsid.h"
+#include "c64/c64cia.h"
+#include "c64/c64vic.h"
 
-class player: private C64Environment
+#include "mos6510/mos6510.h"
+
+class Player: private C64Environment, c64env
 {
 private:
     static const double CLOCK_FREQ_NTSC;
@@ -75,9 +80,11 @@ private:
     static const char  *TXT_PAL_VBI;
     static const char  *TXT_PAL_VBI_FIXED;
     static const char  *TXT_PAL_CIA;
+    static const char  *TXT_PAL_UNKNOWN;
     static const char  *TXT_NTSC_VBI;
     static const char  *TXT_NTSC_VBI_FIXED;
     static const char  *TXT_NTSC_CIA;
+    static const char  *TXT_NTSC_UNKNOWN;
     static const char  *TXT_NA;
 
     static const char  *ERR_CONF_WHILST_ACTIVE;
@@ -86,21 +93,77 @@ private:
     static const char  *ERR_MEM_ALLOC;
     static const char  *ERR_UNSUPPORTED_MODE;
     static const char  *ERR_FILTER_DEFINITION;
+    static const char  *credit[10]; // 10 credits max
 
     //SID6510  cpu(6510, "Main CPU");
-    SID6510 cpu;
-    SID     sid;
-    SID     sid2;
-    XSID    xsid;
-    MOS6526 cia;
-    MOS6526 cia2;
-    MOS656X vic;
+    SID6510 sid6510;
+    MOS6510 mos6510;
+    MOS6510 *cpu;
+    // Sid objects to use.
+    c64resid mos6581_1;
+    c64resid mos6581_2;
+    c64xsid  xsid;
+    c64cia1  cia;
+    c64cia2  cia2;
+    c64vic   vic;
+    sidemu  *sid;
+    sidemu  *sid2;
+
+    class EventMixer: public Event
+    {
+    private:
+        Player &m_player;
+        void event (void) { m_player.mixer (); }
+
+    public:
+        EventMixer (Player *player)
+        :Event("Mixer"),
+         m_player(*player) {}
+    } mixerEvent;
+    friend EventMixer;
+
+    class EventRTC: public Event
+    {
+    private:
+        event_clock_t m_seconds;
+        float64_t     m_period;
+        float64_t     m_fclk;
+        EventContext &m_eventContext;
+
+        void    reset   (void)
+        {   m_seconds  = 0; }
+
+        void event (void)
+        {
+            event_clock_t cycles;
+            m_seconds++;
+            m_fclk += m_period;
+            cycles  = (event_clock_t) (m_fclk);
+            m_fclk -= cycles;
+            m_eventContext.schedule (this, cycles);
+        }
+
+    public:
+        EventRTC (EventContext *context)
+        :Event("RTC"),
+         m_eventContext(*context)
+        {reset ();}
+
+        event_clock_t getTime () {return m_seconds;}
+        void          clock   (float64_t period)
+        {
+            event_clock_t cycles = (event_clock_t) period;
+            reset ();
+            m_period = period;
+            m_fclk   = m_period - cycles;
+            m_eventContext.schedule (this, cycles);
+        }
+    } rtc;
 
     // User Configuration Settings
     struct   SidTuneInfo tuneInfo;
     SidTune *_tune;
-    uint8_t *ram, *rom, psidDrv[0x100];
-    bool     usePsidDrv;
+    uint8_t *ram, *rom;
 
     sid2_clock_t    _clockSpeed;
     sid2_env_t      _environment;
@@ -114,17 +177,22 @@ private:
     uint_least32_t  _seconds;
     int_least32_t   _userLeftVolume;
     int_least32_t   _userRightVolume;
-    enum  {_playing = 0, _paused, _stopped} playerState;
+    volatile enum  {_playing = 0, _paused, _stopped} playerState;
+
+    // Mixer settings
+    float64_t      m_sampleClock;
+    float64_t      m_samplePeriod;
+    uint_least32_t m_sampleCount;
+    uint_least32_t m_sampleIndex;
+    char          *m_sampleBuffer;
 
     // Internal Configuration Settings
+    volatile bool   m_running;
     uint_least8_t   _channels;
-    float64_t       _currentPeriod;
     bool            _digiChannel;
     bool            _forceDualSids;
     sid2_playback_t _playback;
     int             _precision;
-    float64_t       _samplingPeriod;
-    uint_least32_t  _scaleBuffer;
     int_least32_t   _leftVolume;
     int_least32_t   _rightVolume;
 
@@ -134,7 +202,6 @@ private:
     uint_least16_t  _sidAddress[2];
     bool            _sidEnabled[2];
     bool            _filter;
-    bool            _extFilter;
     sid2_model_t    _sidModel;
     bool            _sidSamples;
 
@@ -150,6 +217,8 @@ private:
     void clock          (void);
     int  initialise     (void);
     void nextSequence   (void);
+    void mixer          (void);
+    void mixerReset     (void);
     void mileageCorrect (void)
     {   // If just finished a song, round samples to correct mileage
         if (_sampleCount >= (_samplingFreq / 2))
@@ -157,59 +226,75 @@ private:
         _sampleCount = 0;
     }
 
-    uint8_t readMemByte_player    (uint_least16_t addr, bool useCache);
-    uint8_t readMemByte_plain     (uint_least16_t addr, bool useCache);
-    uint8_t readMemByte_playsid   (uint_least16_t addr, bool useCache);
-    uint8_t readMemByte_sidplaytp (uint_least16_t addr, bool useCache);
-    uint8_t readMemByte_sidplaybs (uint_least16_t addr, bool useCache);
-    void    writeMemByte_plain    (uint_least16_t addr, uint8_t data, bool useCache);
-    void    writeMemByte_playsid  (uint_least16_t addr, uint8_t data, bool useCache);
-    void    writeMemByte_sidplay  (uint_least16_t addr, uint8_t data, bool useCache);
+    uint8_t readMemByte_player    (uint_least16_t addr);
+    uint8_t readMemByte_plain     (uint_least16_t addr);
+    uint8_t readMemByte_playsid   (uint_least16_t addr);
+    uint8_t readMemByte_sidplaytp (uint_least16_t addr);
+    uint8_t readMemByte_sidplaybs (uint_least16_t addr);
+    void    writeMemByte_plain    (uint_least16_t addr, uint8_t data);
+    void    writeMemByte_playsid  (uint_least16_t addr, uint8_t data);
+    void    writeMemByte_sidplay  (uint_least16_t addr, uint8_t data);
 
     // Use pointers to please requirements of all the provided
     // environments.
-    uint8_t (player::*readMemByte)     (uint_least16_t, bool);
-    void    (player::*writeMemByte)    (uint_least16_t, uint8_t, bool);
-    uint8_t (player::*readMemDataByte) (uint_least16_t, bool);
+    uint8_t (Player::*m_readMemByte)    (uint_least16_t);
+    void    (Player::*m_writeMemByte)   (uint_least16_t, uint8_t);
+    uint8_t (Player::*m_readMemDataByte)(uint_least16_t);
+
+    uint8_t  readMemRamByte (const uint_least16_t addr)
+    {   return ram[addr]; }
 
     // Environment Function entry Points
     inline void    envReset           (void);
-    inline uint8_t envReadMemByte     (uint_least16_t addr, bool useCache);
-    inline void    envWriteMemByte    (uint_least16_t addr, uint8_t data, bool useCache);
-    inline void    envTriggerIRQ      (void);
-    inline void    envTriggerNMI      (void);
-    inline void    envTriggerRST      (void);
-    inline void    envClearIRQ        (void);
+    inline uint8_t envReadMemByte     (uint_least16_t addr);
+    inline void    envWriteMemByte    (uint_least16_t addr, uint8_t data);
     inline bool    envCheckBankJump   (uint_least16_t addr);
-    inline uint8_t envReadMemDataByte (uint_least16_t addr, bool useCache);
+    inline uint8_t envReadMemDataByte (uint_least16_t addr);
+
+    void   envLoadFile (char *file)
+    {
+        char name[0x100] = "e:/emulators/c64/games/prgs/";
+        strcat (name, file);
+        strcat (name, ".sid");
+        _tune->load (name);
+        stop ();
+    }
 
     // Rev 2.0.3 Added - New Mixer Routines
-    void (player::*output) (uint_least16_t clock, void *buffer, uint_least32_t &count);
+    uint_least32_t (Player::*output) (char *buffer);
 
     // Rev 2.0.4 (saw) - Added to reduce code size
-    int_least32_t monoOutGenericMonoIn     (uint_least16_t clock, uint_least32_t &count, uint_least8_t bits);
-    int_least32_t monoOutGenericStereoIn   (uint_least16_t clock, uint_least32_t &count, uint_least8_t bits);
-    int_least32_t monoOutGenericStereoRIn  (uint_least16_t clock, uint_least32_t &count, uint_least8_t bits);
-    int_least32_t stereoOutGenericMonoIn   (uint_least16_t clock, uint_least32_t &count, uint_least8_t bits);
-    int_least32_t stereoOutGenericStereoIn (uint_least16_t clock, uint_least32_t &count, uint_least8_t bits,
-                                            int_least32_t &sampleR);
+    int_least32_t monoOutGenericLeftIn   (uint_least8_t  bits);
+    int_least32_t monoOutGenericStereoIn (uint_least8_t  bits);
+    int_least32_t monoOutGenericRightIn  (uint_least8_t bits);
 
     // 8 bit output
-    void monoOut8MonoIn       (uint_least16_t clock, void *buffer, uint_least32_t &count);
-    void monoOut8StereoIn     (uint_least16_t clock, void *buffer, uint_least32_t &count);
-    void monoOut8StereoRIn    (uint_least16_t clock, void *buffer, uint_least32_t &count);
-    void stereoOut8MonoIn     (uint_least16_t clock, void *buffer, uint_least32_t &count);
-    void stereoOut8StereoIn   (uint_least16_t clock, void *buffer, uint_least32_t &count);
+    uint_least32_t monoOut8MonoIn       (char *buffer);
+    uint_least32_t monoOut8StereoIn     (char *buffer);
+    uint_least32_t monoOut8StereoRIn    (char *buffer);
+    uint_least32_t stereoOut8MonoIn     (char *buffer);
+    uint_least32_t stereoOut8StereoIn   (char *buffer);
 
     // Rev 2.0.4 (jp) - Added 16 bit support
-    void monoOut16MonoIn      (uint_least16_t clock, void *buffer, uint_least32_t &count);
-    void monoOut16StereoIn    (uint_least16_t clock, void *buffer, uint_least32_t &count);
-    void monoOut16StereoRIn   (uint_least16_t clock, void *buffer, uint_least32_t &count);
-    void stereoOut16MonoIn    (uint_least16_t clock, void *buffer, uint_least32_t &count);
-    void stereoOut16StereoIn  (uint_least16_t clock, void *buffer, uint_least32_t &count);
+    uint_least32_t monoOut16MonoIn      (char *buffer);
+    uint_least32_t monoOut16StereoIn    (char *buffer);
+    uint_least32_t monoOut16StereoRIn   (char *buffer);
+    uint_least32_t stereoOut16MonoIn    (char *buffer);
+    uint_least32_t stereoOut16StereoIn  (char *buffer);
+
+    void extFilter (uint fc)
+    {
+        double cutoff = fc;
+        mos6581_1.exfilter (cutoff);
+        mos6581_1.exfilter (cutoff);
+    }
+
+    void interruptIRQ (const bool state);
+    void interruptNMI (void);
+    void interruptRST (void);
 
 public:
-    player ();
+    Player ();
 
     int            clockSpeed   (sid2_clock_t clock, bool forced);
     int            configure    (sid2_playback_t mode, uint_least32_t samplingFreq, int precision,
@@ -222,9 +307,11 @@ public:
     void           pause        (void);
     uint_least32_t play         (void *buffer, uint_least32_t length);
     void           stop         (void);
-    uint_least32_t time         (void) { return _seconds; }
+    uint_least32_t time         (void) {return rtc.getTime (); }
     void           sidSamples   (bool enable);
     int            loadFilter   (const sid_fc_t *cutoffs, uint_least16_t points);
+    const char   **credits      (void) {return credit;}
+    void           debug        (bool enable) { cpu->debug (enable); }
 
     void           optimisation (uint_least8_t level)
     {
@@ -237,36 +324,47 @@ public:
     void filter (bool enabled)
     {
         _filter = enabled;
-        sid.enable_filter  (_filter);
-        sid2.enable_filter (_filter);
-    }
-
-    void extFilter (bool enabled)
-    {
-        _extFilter = enabled;
-        sid.enable_external_filter  (_extFilter);
-        sid2.enable_external_filter (_extFilter);
+        sid->filter  (_filter);
+        sid2->filter (_filter);
     }
 
     void sidModel (sid2_model_t model)
     {
         if (_sidModel == model)
-        return;
+            return;
         _sidModel = model;
-        if (_sidModel == SID2_MOS6581)
+        if (model != SID2_MODEL_CORRECT)
         {
-            sid.set_chip_model  (MOS6581);
-            sid2.set_chip_model (MOS6581);
-        }
-        else // if (_sidModel == SID2_MOS8580
-        {
-            sid.set_chip_model  (MOS8580);
-            sid2.set_chip_model (MOS8580);
+            sid->model  (model);
+            sid2->model (model);
         }
     }
 
     const char *getErrorString (void)
     {   return _errorString; }
 };
+
+inline void Player::interruptIRQ (const bool state)
+{
+    if (state)
+    {
+        cpu->triggerIRQ ();
+        // Start the sample sequence
+        xsid.suppress (false);
+        xsid.suppress (true);
+    }
+    else
+        cpu->clearIRQ ();
+}
+
+inline void Player::interruptNMI ()
+{
+    cpu->triggerNMI ();
+}
+
+inline void Player::interruptRST ()
+{
+    stop ();
+}
 
 #endif // _player_h_
