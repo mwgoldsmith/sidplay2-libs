eece7745ab72dbf30800624acb6cc8eed8b50ac3
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-07-25 16:58:35 +0000

    Supports new configuration interface.

--- libsidplay/include/sidplay/sidplay2.h
+++ libsidplay/include/sidplay/sidplay2.h
@@ -22,7 +22,6 @@
 #include "SidTune.h"
 #include "sidbuilder.h"
 
-
 // Default settings
 const uint_least32_t SID2_DEFAULT_SAMPLING_FREQ = 44100;
 const uint_least8_t  SID2_DEFAULT_PRECISION     = 16;
@@ -32,16 +31,6 @@ const bool           SID2_DEFAULT_SID_SAMPLES   = true; // Samples through sid
 // Maximum values
 const uint_least8_t SID2_MAX_PRECISION    = 16;
 const uint_least8_t SID2_MAX_OPTIMISATION = 2;
-
-typedef struct
-{
-    char        *name;
-    char        *version;
-    SidTuneInfo  tuneInfo;
-    bool         filter;
-    sid2_env_t   environment;
-    sid2_model_t sidModel;
-} sid2_playerInfo_t;
 
 // Private Sidplayer
 class Player;
@@ -54,31 +43,22 @@ public:
     sidplay2 ();
     virtual ~sidplay2 ();
 
-    void           configure    (sid2_playback_t mode, uint_least32_t samplingFreq, uint_least8_t precision, bool forceDualSid);
-    void           stop         (void);
+    const sid2_config_t &configure (void);
+    const sid2_info_t   &info      (void);
+
+    int            configure    (const sid2_config_t &cfg);
+    const char    *error        (void);
+    int            fastForward  (uint percent);
+    int            loadSong     (SidTune *tune);
     void           pause        (void);
     uint_least32_t play         (void *buffer, uint_least32_t length);
-    int            loadSong     (SidTune *tune);
-    int            environment  (sid2_env_t env);
-    int            fastForward  (uint_least8_t percent);
-    void           getInfo      (sid2_playerInfo_t *info);
-    void           optimisation (uint_least8_t level);
-    int            loadFilter   (const sid_fc_t *cutoffs, uint_least16_t points);
-    const char   **credits      (void);
-    void           emulation    (sidbuilder *builder);
+    sid2_player_t  state        (void);
+    void           stop         (void);
     void           debug        (bool enable);
 
     // Timer functions with respect to 10ths of a second
     uint_least32_t time    (void);
     uint_least32_t mileage (void);
-    // Added new filter, model, and clockspeed functions
-    void filter     (bool enabled);
-    void sidModel   (sid2_model_t model);
-    void clockSpeed (sid2_clock_t clock, bool forced = true);
-    void sidSamples (bool enable);
-
-    // Rev 1.5 (saw) - Added error loop through support
-    const char *getErrorString (void);
 
     operator bool()  const { return (&sidplayer ? true: false); }
     bool operator!() const { return (&sidplayer ? false: true); }
