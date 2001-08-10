378b34adbabe16bf5cc5de5c1859500efeebf17a
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-08-10 20:55:01 +0000

    Initial release.

---
+++ libsidplay/include/sidplay/sid2types.h
@@ -0,0 +1,68 @@
+/***************************************************************************
+                          sid2types.h  -  sidplay2 specific types
+                             -------------------
+    begin                : Fri Aug 10 2001
+    copyright            : (C) 2001 by Simon White
+    email                : s_a_white@email.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _sid2types_h_
+#define _sid2types_h_
+
+#include "sidtypes.h"
+
+class   sidbuilder;
+struct  SidTuneInfo;
+
+typedef enum {sid2_playing = 0, sid2_paused, sid2_stopped}         sid2_player_t;
+typedef enum {sid2_left  = 0, sid2_mono,  sid2_stereo, sid2_right} sid2_playback_t;
+typedef enum {sid2_envPS = 0, sid2_envTP, sid2_envBS,  sid2_envR } sid2_env_t;
+typedef enum {SID2_MODEL_CORRECT, SID2_MOS6581, SID2_MOS8580}      sid2_model_t;
+typedef enum {SID2_CLOCK_CORRECT, SID2_CLOCK_PAL, SID2_CLOCK_NTSC} sid2_clock_t;
+
+/* Environment Modes
+sid2_envPS = Playsid
+sid2_envTP = Sidplay  - Transparent Rom
+sid2_envBS = Sidplay  - Bankswitching
+sid2_envR  = Sidplay2 - Real C64 Environment
+*/
+
+struct sid2_config_t
+{
+    bool                clockForced;
+    sid2_clock_t        clockSpeed;
+    sid2_env_t          environment;
+    bool                forceDualSids;
+    uint_least32_t      frequency;
+    uint_least8_t       optimisation;
+    sid2_playback_t     playback;
+    int                 precision;
+    sidbuilder         *sidEmulation;
+    bool                sidFilter;
+    const sid_filter_t *sidFilterDef;
+    sid2_model_t        sidModel;
+    bool                sidSamples;
+    uint_least32_t      leftVolume;
+    uint_least32_t      rightVolume;
+};
+
+struct sid2_info_t
+{
+    const char       **credits;
+    uint               channels;
+    uint_least16_t     driverAddr;
+    const char        *name;
+    const SidTuneInfo *tuneInfo;
+    const char        *version;
+};
+
+#endif /* _sid2types_h_ */
