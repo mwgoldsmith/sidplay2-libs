870e94ae021baa25d4e68f8ae6400450a16694b4
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-09-20 19:34:53 +0000

    Initial Release.

---
+++ libsidplay/src/nullsid.h
@@ -0,0 +1,40 @@
+/***************************************************************************
+                          nullsid.h  -  Null SID Emulation
+                             -------------------
+    begin                : Thurs Sep 20 2001
+    copyright            : (C) 2001 by Simon White
+    email                : s_a_white@email.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _nullsid_h_
+#define _nullsid_h_
+
+#include "sidbuilder.h"
+
+class NullSID: public sidemu
+{   // Standard component functions
+    void    reset (void) { ; }
+    uint8_t read  (const uint_least8_t) { return 0; }
+    void    write (const uint_least8_t, const uint8_t) { ; }
+    const   char *credits (void) { return ""; }
+    const   char *error   (void) { return ""; }
+
+    // Standard SID functions
+    int_least32_t output (const uint_least8_t) { return 0; }
+    void          model  (const sid2_model_t) { ; }
+    void          filter (const bool) { ; }
+    void          voice  (const uint_least8_t, const uint_least8_t,
+                          const bool) { ; }
+    void          gain   (const int_least8_t) { ; }
+};
+
+#endif // _nullsid_h_
