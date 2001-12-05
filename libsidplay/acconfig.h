b014fef2b31fc43ed969fb0ff241df086f9e872f
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-12-05 23:47:03 +0000

    Config header files moved.

--- libsidplay/acconfig.h
+++ libsidplay/acconfig.h
@@ -38,10 +38,6 @@
 /* Define if the C++ compiler supports BOOL */
 #undef HAVE_BOOL
 
-#undef VERSION
-
-#undef PACKAGE
-
 /* Define if you need the GNU extensions to compile */
 #undef _GNU_SOURCE
 
