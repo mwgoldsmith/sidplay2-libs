fe2bd9165d7f87c637fbb3581718d870c09c1c6b
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-12-05 23:37:39 +0000

    Now redirects to real sidfig.hy/i

--- libsidplay/include/sidplay/sidconfig.h
+++ libsidplay/include/sidplay/sidconfig.h
@@ -1,43 +1,27 @@
-/* Setup for Microsoft Visual C++ Version 5 */
-#ifndef _sidconfig_h_
-#define _sidconfig_h_
+/***************************************************************************
+                          sidconfig.h  -  Redirect to real sidconfig.h
+                             -------------------
+    begin                : Tues Dec 4 2001
+    copyright            : (C) 2001 by Simon White
+    email                : s_a_white@email.com
+ ***************************************************************************/
 
-/* Define the sizeof of types in bytes */
-#define SID_SIZEOF_CHAR      1
-#define SID_SIZEOF_SHORT_INT 2
-#define SID_SIZEOF_INT       4
-#define SID_SIZEOF_LONG_INT  4
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/***************************************************************************
+ *  $Log: not supported by cvs2svn $
+ ***************************************************************************/
 
-/* Define if your processor stores words with the most significant
-   byte first (like Motorola and SPARC, unlike Intel and VAX).  */
-#define SID_WORDS_LITTLEENDIAN
-
-/* Define if your compiler supports type "bool".
-   If not, a user-defined signed integral type will be used.  */
-#define SID_HAVE_BOOL
-
-/* Define if your compiler supports AC99 header "stdint.h" */
-//#define SID_HAVE_STDINT_H
-
-/* Define if your compiler supports AC99 header "stdbool.h" */
-//#define SID_HAVE_STDBOOL_H
-
-#ifdef _MSC_VER
-    /* Microsoft specific compiler problem */
-#   define SID_HAVE_BAD_COMPILER
+#if defined(HAVE_UNIX)
+#   include "../../unix/sidconfig.h"
+#elif defined(HAVE_WINDOWS)
+#   include "../../win/VC/sidconfig.h"
+#else
+#   error Platform not supported!
 #endif
-
-/* DLL building support on win32 hosts */
-#ifndef SID_EXTERN
-#   ifdef DLL_EXPORT      /* defined by libtool (if required) */
-#       define SID_EXTERN __declspec(dllexport)
-#   endif
-#   ifdef SID_DLL_IMPORT  /* define if linking with this dll */
-#       define SID_EXTERN __declspec(dllimport)
-#   endif
-#   ifndef SID_EXTERN     /* static linking or !_WIN32 */
-#       define SID_EXTERN
-#   endif
-#endif
-
-#endif // _sidconfig_h_
