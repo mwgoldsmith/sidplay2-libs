c4baf7783513c9631bdf99ab0f8c6be776f62da7
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-12-06 18:26:17 +0000

    Support new include structure.

--- libsidutils/acconfig.h
+++ libsidutils/acconfig.h
@@ -4,19 +4,12 @@
 
 /* @FOO@ : Define or undefine value FOO as appropriate. */
 
-/* Operating System */
-#define HAVE_UNIX
-
 /* Define if your C++ compiler implements exception-handling.  */
 #undef HAVE_EXCEPTIONS
 @TOP@
 
 /* Define if the C++ compiler supports BOOL */
 #undef HAVE_BOOL
-
-#undef VERSION
-
-#undef PACKAGE
 
 /* Define if you need the GNU extensions to compile */
 #undef _GNU_SOURCE
