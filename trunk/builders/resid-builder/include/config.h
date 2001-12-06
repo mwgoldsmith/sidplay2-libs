afcc12d477b0f39ff3d9db20b8b6e19bc45ac3a6
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2001-12-06 23:45:18 +0000

    Now redirects to real config.h

--- trunk/builders/resid-builder/include/config.h
+++ trunk/builders/resid-builder/include/config.h
@@ -1,27 +1,37 @@
-/* Setup for Microsoft Visual C++ Version 5 */
+/* include/config.h.  Generated automatically by configure.  */
+/* include/config.h.in.  Generated automatically from configure.in by autoheader.  */
+/* config.h (template) */
 #ifndef _config_h_
 #define _config_h_
 
-/* Operating System */
-#define HAVE_MSWINDOWS
+/* @FOO@ : Define or undefine value FOO as appropriate. */
 
 /* Define if your C++ compiler implements exception-handling.  */
-/* #define HAVE_EXCEPTIONS 1 */
+#define HAVE_EXCEPTIONS 1
+
+/* Defines to indicate that resid is in an non standard location.
+   HAVE_LOCAL_RESID indicates that an include path has been used
+   to where the resid directory is located but does not include
+   the resid directory name.
+   HAVE_USER_RESID indicates that a fully qualified include path
+   has been provided that includes the resid directory name and therefore
+   resids include files are available by calling them directly.
+*/
+/* #undef HAVE_LOCAL_RESID */
+/* #undef HAVE_USER_RESID */
+
+
+
+/* Define if the C++ compiler supports BOOL */
+/* #undef HAVE_BOOL */
+
+
+/* Define if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
 
 /* Name of package */
 #define PACKAGE "resid-builder"
 
 /* Version number of package */
-#define VERSION "0.0.1"
-
-/* Defines to indicate that resid is in an non standard location.
-   SID_HAVE_LOCAL_RESID indicates that an include path has been used
-   to where the resid directory is located but does not include
-   the resid directory name.
-   SID_HAVE_USER_RESID indicates that a fully qualified include path
-   has been provided that includes the resid directory name and therefore
-   resids include files are available by calling them directly.
-*/
-#define HAVE_LOCAL_RESID
-
-#endif // _config_h_
+#define VERSION "1.0.0"
+#endif /* _config_h_ */
