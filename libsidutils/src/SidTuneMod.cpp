3841fe4586acee111a56cc920d10865a52a9e66c
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2002-09-10 21:47:32 +0000

    PSIDv2NG changes for MD5 calculation.

--- libsidutils/src/SidTuneMod.cpp
+++ libsidutils/src/SidTuneMod.cpp
@@ -22,12 +22,9 @@
 void SidTuneMod::createMD5(MD5& myMD5)
 {
     if (status)
-    {
-        // Include C64 data.
-        uint_least16_t currentSong = info.currentSong;
+    {   // Include C64 data.
         md5_byte_t tmp[2];
-
-        myMD5.append(cache.get()+fileOffset,info.c64dataLen);
+        myMD5.append (cache.get()+fileOffset,info.c64dataLen);
         // Include INIT and PLAY address.
         endian_little16 (tmp,info.initAddr);
         myMD5.append    (tmp,sizeof(tmp));
@@ -36,14 +33,30 @@ void SidTuneMod::createMD5(MD5& myMD5)
         // Include number of songs.
         endian_little16 (tmp,info.songs);
         myMD5.append    (tmp,sizeof(tmp));
-        // Include song speed for each song.
-        for (uint_least16_t s = 1; s <= info.songs; s++)
-        {
-            selectSong (s);
-            myMD5.append (&info.songSpeed,sizeof(info.songSpeed));
+        {   // Include song speed for each song.
+            uint_least16_t currentSong = info.currentSong;
+            for (uint_least16_t s = 1; s <= info.songs; s++)
+            {
+                selectSong (s);
+                myMD5.append (&info.songSpeed,sizeof(info.songSpeed));
+            }
+            // Restore old song
+            selectSong (currentSong);
         }
-        // Restore old song
-        selectSong (currentSong);
+        // Deal with PSID v2NG clock speed flags: Let only NTSC
+        // clock speed change the MD5 fingerprint. That way the
+        // fingerprint of a PAL-speed sidtune in PSID v1, v2, and
+        // PSID v2NG format is the same.
+        if (info.clockSpeed == SIDTUNE_CLOCK_NTSC)
+            myMD5.append (&info.clockSpeed,sizeof(info.clockSpeed));
+        // NB! If the fingerprint is used as an index into a
+        // song-lengths database or cache, modify above code to
+        // allow for PSID v2NG files which have clock speed set to
+        // SIDTUNE_CLOCK_ANY. If the SID player program fully
+        // supports the SIDTUNE_CLOCK_ANY setting, a sidtune could
+        // either create two different fingerprints depending on
+        // the clock speed chosen by the player, or there could be
+        // two different values stored in the database/cache.
     }
 }
 
