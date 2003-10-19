223e9ccd711e7351add7f8cd30ca6a9eb5570ad2
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2003-10-19 23:29:30 +0000

    Added support for older MM input format and MAP (human readable) output format.

--- trunk/libsidutils/include/sidplay/utils/SidUsage.h
+++ trunk/libsidutils/include/sidplay/utils/SidUsage.h
@@ -18,22 +18,46 @@
 #ifndef _SidUsage_h_
 #define _SidUsage_h_
 
-#include <sidplay/sidusage.h>
-#include <sidplay/SidTune.h>
+struct SidTuneInfo;
+struct sid_usage_t;
+struct IffHeader;
+struct Chunk;
 
 
 class SID_EXTERN SidUsage
 {
+private:
+    char m_decodeMAP[0x100][3];
+    // Ignore errors
+    uint_least8_t m_filterMAP[0x10000];
+
 protected:
     bool  m_status;
     const char *m_errorString;
+
+private:
+    // Old obsolete MM file format
+    bool           readMM     (FILE *file, sid_usage_t &usage, const char *ext);
+    // Sid Memory Map (MM file)
+    bool           readSMM    (FILE *file, sid_usage_t &usage, const char *ext);
+    bool           readSMM0   (FILE *file, sid_usage_t &usage,
+                               const IffHeader &header);
+    void           writeSMM0  (FILE *file, const sid_usage_t &usage);
+    void           writeMAP   (FILE *file, const sid_usage_t &usage);
+    void           filterMAP  (int from, int to, uint_least8_t mask);
+
+protected:
+    uint_least32_t readChunk  (FILE *file, Chunk &chunk);
+    bool           writeChunk (FILE *file, const Chunk &chunk,
+                               uint_least32_t type,
+                               uint_least32_t length = 0);
+    uint_least32_t skipChunk  (FILE *file);
 
 public:
     SidUsage ();
 
     void           read       (const char *filename, sid_usage_t &usage);
-    void           write      (const char *filename, const sid_usage_t &usage,
-                               SidTuneInfo &tuneInfo);
+    void           write      (const char *filename, const sid_usage_t &usage);
     const char *   error      (void) { return m_errorString; }
 
     operator bool () { return m_status; }
