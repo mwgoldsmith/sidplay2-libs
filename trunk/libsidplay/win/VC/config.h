905589a52aa719f77b80312fc653e4bd1baadb8e
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2004-03-09 20:46:28 +0000

    Switch to using SSTREAM and update the version number

--- trunk/libsidplay/win/VC/config.h
+++ trunk/libsidplay/win/VC/config.h
@@ -3,19 +3,21 @@
 #define _config_h_
 
 #define PACKAGE "libsidplay"
-#define VERSION "2.1.0a3"
+#define VERSION "2.1.1"
+
+#define S_A_WHITE_EMAIL "sidplay2@yahoo.com"
 
 /* Define if your C++ compiler implements exception-handling.  */
 /* Note: exception specification is only available for MSVC > 6 */
 #if _MSC_VER > 1200
-#  define HAVE_EXCEPTIONS
+#   define HAVE_EXCEPTIONS
 #endif
 
 /* Define if you support file names longer than 14 characters.  */
 #define HAVE_LONG_FILE_NAMES
 
-/* Define if you have the <strstrea.h> header file.  */
-#define HAVE_STRSTREA_H
+/* Define if you have the <sstream> header file.  */
+#define HAVE_SSTREAM
 
 /* Define if ``ios::nocreate'' is supported. */
 //#define HAVE_IOS_NOCREATE
