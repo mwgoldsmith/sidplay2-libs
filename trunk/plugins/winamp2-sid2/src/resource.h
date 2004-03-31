cfd737af941225bec8aa563ab80c2f41d86e164e
vividos <vividos@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2004-03-31 21:35:08 +0000

    reordered control IDs

--- trunk/plugins/winamp2-sid2/src/resource.h
+++ trunk/plugins/winamp2-sid2/src/resource.h
@@ -5,26 +5,26 @@
 #define IDD_DIALOG_INFO                 101
 #define IDD_DIALOG_CONFIG               102
 #define IDD_DIALOG_ABOUT                103
-#define IDC_EDIT_DETAILS                1002
-#define IDC_EDIT_INFOSTR                1003
-#define IDC_EDIT_SIDCREDITS             1005
-#define IDC_RADIO_OPT_LEVEL1            1006
-#define IDC_RADIO_OPT_LEVEL2            1007
-#define IDC_STATIC_VERSIONS             1009
-#define IDC_EDIT_TITLE_FMT              1010
-#define IDC_COMBO_SAMPLERATE            1011
-#define IDC_COMBO_SIDMODEL              1012
-#define IDC_CHECK_STEREO                1013
-#define IDC_CHECK_SIDFILTER             1014
-#define IDC_BUTTON1                     1015
+#define IDC_EDIT_DETAILS                1001
+#define IDC_EDIT_INFOSTR                1002
+#define IDC_EDIT_SIDCREDITS             1003
+#define IDC_RADIO_OPT_LEVEL1            1004
+#define IDC_RADIO_OPT_LEVEL2            1005
+#define IDC_STATIC_VERSIONS             1006
+#define IDC_EDIT_TITLE_FMT              1007
+#define IDC_COMBO_SAMPLERATE            1008
+#define IDC_COMBO_SIDMODEL              1009
+#define IDC_CHECK_STEREO                1010
+#define IDC_CHECK_SIDFILTER             1011
+#define IDC_BUTTON_UNINSTALL            1012
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1016
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
