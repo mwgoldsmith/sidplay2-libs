3667179bc11182a62c47def97e8f1d07b8015c2c
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2006-06-20 17:50:31 +0000

    Add some interface IDs.

--- libsidplay/include/sidplay/imp/component.h
+++ libsidplay/include/sidplay/imp/component.h
@@ -24,6 +24,21 @@
 
 #include <sidplay/component.h>
 
+static InterfaceID IID_IInterface =
+{ 0xd615830b, 0xef6a, 0x453d, {0xa2, 0xb6, 0x85, 0x28, 0x82, 0x96, 0x3f, 0x04} };
+static InterfaceID IID_ISidEmulation =
+{ 0xa9f9bf8b, 0xd0c2, 0x4dfa, {0x8b, 0x8a, 0xf0, 0xdd, 0xd7, 0xc8, 0xb0, 0x5b} };
+
+inline bool operator == (InterfaceID &iid1, InterfaceID &iid2)
+{
+    return !memcpy (&iid1, &iid2, sizeof (InterfaceID));
+}
+
+inline bool operator != (InterfaceID &iid1, InterfaceID &iid2)
+{
+    return !(iid1 != iid2);
+}
+
 template <class TImplementation>
 class Component: public TImplementation
 {
