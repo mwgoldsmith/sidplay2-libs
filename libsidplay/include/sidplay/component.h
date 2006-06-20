ca4a5d4fe6f402beb5fbade561c6f9fdb6e7cf61
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2006-06-20 22:14:47 +0000

    Export IID's in interface files for now.

--- libsidplay/include/sidplay/component.h
+++ libsidplay/include/sidplay/component.h
@@ -31,6 +31,9 @@ struct InterfaceID
 #define IF_QUERY(Interface,Implementation) IID_##Interface, \
     reinterpret_cast<void**>(static_cast<Interface **>(Implementation))
 
+static const InterfaceID IID_IInterface =
+{ 0xd615830b, 0xef6a, 0x453d, {0xa2, 0xb6, 0x85, 0x28, 0x82, 0x96, 0x3f, 0x04} };
+
 class IInterface
 {
 public:
@@ -39,6 +42,9 @@ public:
     virtual void ifquery   (const InterfaceID &cid, void **implementation) = 0;
     virtual void ifrelease () = 0;
 };
+
+static const InterfaceID IID_IComponent =
+{ 0xa9f9bf8b, 0xd0c2, 0x4dfa, {0x8b, 0x8a, 0xf0, 0xdd, 0xd7, 0xc8, 0xb0, 0x5b} };
 
 class IComponent : public IInterface
 {
