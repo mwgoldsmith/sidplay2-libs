1288b122d26f507fa0e1ddd1dd419c2f98c4f7b8
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2006-10-31 19:46:00 +0000

    Changes IID to SID2IID for compatibiliy with Windows.

--- libsidplay/include/sidplay/iinterface.h
+++ libsidplay/include/sidplay/iinterface.h
@@ -32,7 +32,7 @@ template<uint_least32_t d1,  uint_least16_t d2,  uint_least16_t d3,
          uint_least8_t d4_1, uint_least8_t d4_2, uint_least8_t d4_3,
          uint_least8_t d4_4, uint_least8_t d4_5, uint_least8_t d4_6,
          uint_least8_t d4_7, uint_least8_t d4_8>
-static const InterfaceID &IID ()
+static const InterfaceID &SID2IID ()
 {
     static const InterfaceID iid =
         {d1, d2, d3, {d4_1, d4_2, d4_3, d4_4, d4_5, d4_6, d4_7, d4_8} };
@@ -46,7 +46,7 @@ protected:
 
 public:
     static const InterfaceID &iid () {
-        return IID<0xd615830b, 0xef6a, 0x453d, 0xa2, 0xb6, 0x85, 0x28, 0x82, 0x96, 0x3f, 0x04>();
+        return SID2IID<0xd615830b, 0xef6a, 0x453d, 0xa2, 0xb6, 0x85, 0x28, 0x82, 0x96, 0x3f, 0x04>();
     }
 
     // IInterface
@@ -66,7 +66,7 @@ inline IInterface::~IInterface () { ; }
 // Query an interface (new method)
 // The one and only real function.  Do not modify or add new
 // non virtual functions.  You will break binary compatibility
-template<class T, class U> inline T *if_cast (U unknown)
+template<class T, class U> inline T *if_cast (U *unknown)
 {
     T *implementation = 0;
     unknown->ifquery (IF_QUERY(T, &implementation));
