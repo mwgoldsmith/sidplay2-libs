7f5cbda140ff300cf6eab14f4e3a49c7f28c363b
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2012-01-08 22:15:34 +0000

    Almost all logic is now working.  Add hex entries and file menu actions.

--- sidedit/src/sidedit.cpp
+++ sidedit/src/sidedit.cpp
@@ -1,3 +1,7 @@
+#include <QCloseEvent>
+#include <QFileDialog>
+#include <QMessageBox>
+
 #include "sidedit.h"
 #include "ui_sidedit.h"
 
@@ -52,7 +56,7 @@ SidEdit::SIDTypeInfo::SIDTypeInfo ()
 SidEdit::TimingInfo::TimingInfo ()
 {
     _add (SIDTUNE_SPEED_VBI,    QT_TR_NOOP("VIC (Vertical Blank Interrupt)"));
-    _add (SIDTUNE_SPEED_CIA_1A, QT_TR_NOOP("CIA (60 Hz)"));
+    _add (SIDTUNE_SPEED_CIA_1A, QT_TR_NOOP("CIA (Default 60 Hz)"));
 }
 
 SidEdit::VICSpeedInfo::VICSpeedInfo ()
@@ -65,6 +69,7 @@ SidEdit::VICSpeedInfo::VICSpeedInfo ()
 
 SidEdit::SidEdit(QWidget *parent, Qt::WFlags flags)
 :QMainWindow(parent, flags)
+,m_isModified(false)
 ,m_playerTypePSID(false)
 ,m_playerTypeRSID(true)
 ,m_setup(0)
@@ -82,7 +87,14 @@ SidEdit::SidEdit(QWidget *parent, Qt::WFlags flags)
     m_txtCreditTitle    = _find<QLineEdit>("txtCreditTitle");
     m_txtCreditAuthor   = _find<QLineEdit>("txtCreditAuthor");
     m_txtCreditReleased = _find<QLineEdit>("txtCreditReleased");
+    m_txtLoadRange      = _find<QLabel>("txtLoadRange");
     m_sbrSong           = _find<QScrollBar>("sbrSong");
+    m_spnInitAddress    = _find<HexSpinBox>("spnInitAddress");
+    m_spnLoadAddress    = _find<HexSpinBox>("spnLoadAddress");
+    m_spnPlayAddress    = _find<HexSpinBox>("spnPlayAddress");
+    m_spnPlayerStart    = _find<HexSpinBox>("spnPlayerStart");
+    m_spnPlayerEnd      = _find<HexSpinBox>("spnPlayerEnd");
+    m_spnVersion        = _find<QSpinBox>("spnVersion");
     m_spnSongs          = _find<QSpinBox>("spnSongs");
     m_spnVersion        = _find<QSpinBox>("spnVersion");
     m_txtPlayerPages    = _find<QLabel>("txtPlayerPages");
@@ -104,7 +116,11 @@ SidEdit::SidEdit(QWidget *parent, Qt::WFlags flags)
     connect (this, SIGNAL(_player(bool)), _find<QObject>("lblPlayerStart"), SLOT(setEnabled(bool)));
     connect (this, SIGNAL(_player(bool)), _find<QObject>("lblPlayerEnd"),   SLOT(setEnabled(bool)));
     connect (this, SIGNAL(_player(bool)), _find<QObject>("lblPlayerRange"), SLOT(setEnabled(bool)));
+    connect (this, SIGNAL(_player(bool)), m_spnPlayerStart, SLOT(setEnabled(bool)));
+    connect (this, SIGNAL(_player(bool)), m_spnPlayerEnd,   SLOT(setEnabled(bool)));
     connect (this, SIGNAL(_player(bool)), m_txtPlayerPages, SLOT(setEnabled(bool)));
+    connect (m_spnPlayerStart, SIGNAL(valueChanged(int)), SLOT (_playerStartChanged(int)));
+    connect (m_spnPlayerEnd,   SIGNAL(valueChanged(int)), SLOT (_playerEndChanged(int)));
 
     connect (this, SIGNAL(_subtune(bool)), _find<QObject>("lblCreditTitle"),    SLOT(setEnabled(bool)));
     connect (this, SIGNAL(_subtune(bool)), _find<QObject>("lblCreditAuthor"),   SLOT(setEnabled(bool)));
@@ -135,9 +151,19 @@ SidEdit::SidEdit(QWidget *parent, Qt::WFlags flags)
 //QByteArray outputData = encoder->fromUnicode(result);
 }
 
-SidEdit::~SidEdit()
+SidEdit::~SidEdit ()
 {
     ;
+}
+
+void SidEdit::closeEvent (QCloseEvent *event)
+{
+    if (_maybeSave())
+    {
+        event->accept();
+        return;
+    }
+    event->ignore();
 }
 
 void SidEdit::_defaultClicked ()
@@ -162,8 +188,12 @@ void SidEdit::_fileTypeChanged (int index)
         m_spnVersion->setValue   (2);
         break;
     case ftRSID:
+        {   // RSID must use VIC timing and re-program the hardware for CIA
+            int index = m_timingInfo.indexOf (SIDTUNE_SPEED_VBI);
+            m_cboTiming->setCurrentIndex (index);
+        }
         m_spnSongs->setMaximum   (256);
-        m_timingMaxSong         = 32;
+        m_timingMaxSong         = 0;
         _updateComboBox (m_cboPlayerType, m_playerTypeRSID);
         m_setup = &SidEdit::_setupRSID;
         m_spnVersion->setMinimum (2);
@@ -176,15 +206,157 @@ void SidEdit::_fileTypeChanged (int index)
     _timingEnabled  (m_sbrSong->value() <= m_timingMaxSong);
 }
 
+void SidEdit::_loadFile (const QString &fileName)
+{
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+
+    QApplication::restoreOverrideCursor();
+
+/*
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("Application"),
+                             tr("Cannot read file %1:\n%2.")
+                             .arg(fileName)
+                             .arg(file.errorString()));
+        return;
+    }
+*/
+    setWindowFilePath (fileName);
+}
+
 void SidEdit::_new ()
 {
-    m_sbrSong->setValue  (1);
-    m_spnSongs->setValue (1);
-    _updateComboBox (m_cboFileType, m_fileTypeInfo);
-    _updateComboBox (m_cboSidType,  m_sidTypeInfo);
-    _updateComboBox (m_cboTiming,   m_timingInfo);
-    _updateComboBox (m_cboVicSpeed, m_vicSpeedInfo);
-    _defaultClicked ();
+    if (_maybeSave())
+    {
+        setWindowFilePath ("");
+        m_sbrSong->setValue  (1);
+        m_spnSongs->setValue (1);
+        _updateComboBox (m_cboFileType, m_fileTypeInfo);
+        _updateComboBox (m_cboSidType,  m_sidTypeInfo);
+        _updateComboBox (m_cboTiming,   m_timingInfo);
+        _updateComboBox (m_cboVicSpeed, m_vicSpeedInfo);
+        _defaultClicked ();
+
+        // Load Address
+        m_spnLoadAddress->setValue (0);
+        m_txtLoadRange->setText    ("1 (End: 0x0000)");
+        m_spnInitAddress->setValue (0);
+        m_spnPlayAddress->setValue (0);
+
+        // Player Details
+        m_spnPlayerStart->setValue (0);
+        m_spnPlayerEnd->setValue   (0);
+        _pages (0, 0);
+
+        // Credits
+        m_txtCreditTitle->setText    ("<?>");
+        m_txtCreditAuthor->setText   ("<?>");
+        m_txtCreditReleased->setText ("<?>");
+
+        m_isModified = false;
+    }
+}
+
+bool SidEdit::_maybeSave ()
+{
+    if (m_isModified)
+    {
+        QMessageBox::StandardButton ret;
+        ret = QMessageBox::warning(this, tr("Application"),
+                     tr("The document has been modified.\n"
+                        "Do you want to save your changes?"),
+                     QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+        if (ret == QMessageBox::Save)
+            return _save ();
+        else if (ret == QMessageBox::Cancel)
+            return false;
+    }
+    return true;
+}
+
+void SidEdit::_open()
+{
+    if (_maybeSave())
+    {
+        QString fileName = QFileDialog::getOpenFileName (this);
+        if (!fileName.isEmpty())
+            _loadFile (fileName);
+    }
+}
+
+void SidEdit::_pages (int start, int end)
+{
+    QString str;
+    int offset = 0, length = 0;
+
+    if ((start | end) == 0)
+        str += QT_TR_NOOP("Auto");
+    else
+    {
+        offset  = ((start + 0xff) >> 8);
+        length  = ((end   - 0xff) >> 8);
+        length -= offset;
+        if ((end < start) || (length < 0))
+        {
+            m_txtPlayerPages->setText (QT_TR_NOOP("Illegal"));
+            return;
+        }
+
+        str += QString::number (length + 1);
+    }
+
+    str += " (";
+    str += QT_TR_NOOP("Code");
+    str += ": ";
+    str += m_spnPlayerStart->prefix ();
+    str += QString::number (offset, 16).rightJustified(2, '0').toUpper ();
+    str += QString::number (length, 16).rightJustified(2, '0').toUpper ();
+    str += ')';
+    m_txtPlayerPages->setText (str);
+}
+
+void SidEdit::_playerEndChanged (int end)
+{
+    _pages (m_spnPlayerStart->value(), end);
+}
+
+void SidEdit::_playerStartChanged (int start)
+{
+    _pages (start, m_spnPlayerEnd->value());
+}
+
+bool SidEdit::_save()
+{
+    if (windowFilePath().isEmpty())
+        return _saveAs ();
+    return _saveFile (windowFilePath());
+}
+
+bool SidEdit::_saveAs ()
+{
+    QString fileName = QFileDialog::getSaveFileName (this);
+    if (fileName.isEmpty())
+        return false;
+    return _saveFile (fileName);
+}
+
+bool SidEdit::_saveFile (QString fileName)
+{
+    if (fileName.isEmpty())
+        fileName = "untitled";
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+
+    QApplication::restoreOverrideCursor();
+
+/*
+    QMessageBox::warning(this, tr("Application"),
+                         tr("Cannot write file %1:\n%2.")
+                         .arg(fileName)
+                         .arg(file.errorString()));
+*/
+
+    setWindowFilePath (fileName);
+    return true;
 }
 
 void SidEdit::_setupPSID (int version)
@@ -196,7 +368,6 @@ void SidEdit::_setupPSID (int version)
     emit _player (version2);
     emit _player ((int)version2);
     emit _subtuneHwInfo (m_hwInfo && (m_sbrSong->value() == 1));
-    m_txtPlayerPages->setText (QT_TR_NOOP("Auto")); // @FIXME@
 
     if (!version2)
     {
@@ -213,6 +384,8 @@ void SidEdit::_setupRSID (int version)
     emit _player (true);
     emit _player ((int)true);
     emit _subtuneHwInfo (m_sbrSong->value() == 1);
+    m_spnInitAddress->setValue (0);
+    m_spnPlayAddress->setValue (0);
 }
 
 void SidEdit::_songChanged (int song)
