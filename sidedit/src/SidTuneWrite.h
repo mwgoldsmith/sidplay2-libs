3084c87e178f7296082f54519963de2e559771e4
s_a_white <s_a_white@36447c0f-d6ae-4bf8-b439-2de78d4f2844>
2012-01-22 19:30:16 +0000

    First pass at write support

---
+++ sidedit/src/SidTuneWrite.h
@@ -0,0 +1,27 @@
+#ifndef SIDTUNEWRITE_H
+#define SIDTUNEWRITE_H
+
+#include <SidTune.h>
+
+class SidTuneWrite: public SidTune
+{
+public:
+    SidTuneWrite (const char* fileName, const char **fileNameExt = 0,
+                  const bool separatorIsSlash = false);
+
+    // Load a single-file sidtune from a memory buffer.
+    // Currently supported: PSID format
+    SidTuneWrite (const uint_least8_t* oneFileFormatSidtune, const uint_least32_t sidtuneLength);
+
+    // Error messages stored in getInfo().statusString
+    virtual bool setInfo (const SidTuneInfo &info);
+
+private:
+    static const char SidTuneWrite::txt_vbiClockSpeed[];
+    static const char SidTuneWrite::txt_invalidClockSpeed[];
+    static const char SidTuneWrite::txt_invalidCompatibility[];
+    static const char SidTuneWrite::txt_invalidSidModel[];
+    static const char SidTuneWrite::txt_invalidSongSpeed[];
+};
+
+#endif // SIDTUNEWRITE_H
